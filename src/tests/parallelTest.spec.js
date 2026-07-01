ef67570cff2c50d79ffb2938d2c94622f516e5bb
Wasif <wasif@Wasifs-MacBook-Air.local>
2026-07-02 01:37:37 +0530

    Initial commit

--- src/tests/parallelTest.spec.js
+++ src/tests/parallelTest.spec.js
@@ -4,13 +4,13 @@ test.describe.configure({mode:'parallel'})
 
 test('Parallel Test 1', async({page}) => {
     await page.goto(`https://www.saucedemo.com/`)
-    var loginButton = page.locator('#login-button00');
+    var loginButton = page.locator('#login-button');
     await expect(loginButton).toBeVisible();
 })
 
 test('Parallel Test 2', async({page}) => {
     await page.goto(`https://www.saucedemo.com/`)
-    var loginButton = page.locator('#login-button00');
+    var loginButton = page.locator('#login-button');
     await expect(loginButton).toBeVisible();
 })
 
